e867ede09f65dd690cb2a29c08382a166c0d4616
DLSiem <pdlsiem17@gmail.com>
2024-09-25 18:35:09 +0530

    start setup

---
+++ reduxEssentials/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <p>
+        The sample project we'll look at is a small counter application that
+        lets us add or subtract from a number as we click buttons. It may not be
+        very exciting, but it shows all the important pieces of a React+Redux
+        application in action.
+      </p>
+    </>
+  );
+}
+
+export default App;
